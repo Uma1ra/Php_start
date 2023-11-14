7df3b603138e6ad0ef9773d547acf00703497031
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-11-14 15:12:06 +0900

    [add] story for my variables

--- site.php
+++ site.php
@@ -3,13 +3,21 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="sty">
   <title>PHP Start</title>
 </head>
 <body>
   <?php
-  echo "<h1>This is my PHP Site</h1>";
+
+  echo "<h1>My PHP Site</h1>";
   echo "<hr>";
-  echo "<p>this is going to be the content</p>";
+  echo "<p>This site is my testing ground for PHP learning</p>";
+
+  echo "There once was a man named John </br>";
+  echo "He was 60 years old </br>";
+  echo "He really liked the name John </br>";
+  echo "But didn't like being 60 </br>";
+
   ?>
 </body>
 </html>
