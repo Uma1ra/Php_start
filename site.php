dfa25454427aa9e68c0ecff0cf8e3ad7ff54c653
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-11-14 14:47:00 +0900

    [add] setup php to work on live server
    added live server extension on chrome
    set actual address to the directory
    set the live server address
    opened php in browser

--- site.php
+++ site.php
@@ -3,11 +3,11 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>PHP Start</title>
 </head>
 <body>
   <?php
-  echo "welcome to php <br/>";
+  echo "welcome to php<br/>";
   echo date("Y-m-d H:i:s");
   ?>
 </body>
